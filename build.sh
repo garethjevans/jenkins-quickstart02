83d00f63edcab44d5cfd17ce72828479ac5fc50e
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-09-25 09:51:13 +0100

    basic build working

--- build.sh
+++ build.sh
@@ -7,28 +7,20 @@ set -o pipefail
 ORG=garethjevans
 TAG=$1
 
-DIRS=`find . -name 'env.yaml'`
-for DIR in ${DIRS}; do
-  BUILD_DIR=$(dirname $(dirname ${DIR}))
-  TEAM=$(basename ${BUILD_DIR})
+TEAM=$(basename ${PWD})
+echo "TEAM=${TEAM}"
 
-  echo "TEAM=${TEAM}"
+if [ -f Dockerfile ]; then
+  BUILD_TAG=${ORG}/${TEAM}:${TAG}
+  docker build -t ${BUILD_TAG} .
+  docker push ${BUILD_TAG}
+fi
 
-  pushd ${BUILD_DIR}
-    if [ -f Dockerfile ]; then
-      BUILD_TAG=${ORG}/jenkinsx:${TEAM}_${TAG}
-      docker build -t ${BUILD_TAG} .
-      docker push ${BUILD_TAG}
-    fi
-
-    if [ -f Chart.yaml ]; then
-      sed -i "" -e "s/ImageTag: .*/ImageTag: \"${TEAM}_${TAG}\"/" values.yaml
-      rm -fr charts
-      helm init --client-only
-      helm lint
-      helm dependency update
-      helm template .
-    fi
-  popd
-
-done
+if [ -f Chart.yaml ]; then
+  sed -i "" -e "s/ImageTag: .*/ImageTag: \"${TAG}\"/" values.yaml
+  rm -fr charts
+  helm init --client-only
+  helm lint
+  helm dependency update
+  helm template .
+fi
